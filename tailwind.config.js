2072acc5c2e2d5776586c1bdabfa007f9d05e92f
eduardomarreiro <eduardo4pc@outlook.com>
2023-02-25 15:01:59 -0300

    Adding header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html.cs}"],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },

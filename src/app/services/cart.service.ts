dff0744eb545e748a21510cf5c6fc385199743a9
eduardomarreiro <eduardo4pc@outlook.com>
2023-02-28 13:37:42 -0300

    Implementing cart logic part 2

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -25,6 +25,18 @@ export class CartService {
     this.cart.next({ items})
     this._snackBar.open('1 item added to cart.', 'Ok', {duration: 2500});
     console.log(this. cart.value);
+  }
 
+  getTotal( items: Array<CartItem>) : number {
+    return items
+    .map((item) => item.price * item.quantity)
+    .reduce((prev, current) => prev + current, 0)
+  }
+  
+  clearCart() : void {
+    this.cart.next({ items: []});
+    this._snackBar.open('Cart is cleared.', 'Ok', {
+      duration: 2500
+    });
   }
 }
